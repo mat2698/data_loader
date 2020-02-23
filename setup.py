7551e17afd8c4b38d92656df411afec81e6f97b5
martinmat98 <martin.mat@husky.neu.edu>
2020-02-23 16:03:49 -0500

    requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=['pandas', 'pandas_datareader', 'datetime', 'pickle'],
+    install_requires=['pandas', 'pandas_datareader', 'datetime'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
